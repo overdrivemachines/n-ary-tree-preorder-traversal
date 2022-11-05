4f3f2e37b386ff4e51cbc473bb59d12063694bee
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-04 17:25:50 -0700

    setup basic structure

--- n-ary-tree-preorder-traversal.cpp
+++ n-ary-tree-preorder-traversal.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+// Definition for a Node.
+class Node {
+ public:
+  int val;
+  vector<Node *> children;
+
+  Node() {}
+
+  Node(int _val) {
+    val = _val;
+  }
+
+  Node(int _val, vector<Node *> _children) {
+    val = _val;
+    children = _children;
+  }
+};
+
+vector<int> preorder(Node *root);
+
 int main(int argc, char const *argv[]) {
   return 0;
+}
+
+vector<int> preorder(Node *root) {
 }
