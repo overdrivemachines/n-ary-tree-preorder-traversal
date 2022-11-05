dc8a67e495babda1e184b32f398a748cfb727144
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-04 21:08:34 -0700

    finding bugs in preorder

--- n-ary-tree-preorder-traversal.cpp
+++ n-ary-tree-preorder-traversal.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <stack>
 #include <vector>
 
 using namespace std;
@@ -24,19 +25,44 @@ class Node {
 
 Node *createTree(vector<int> input);
 vector<int> preorder(Node *root);
+void printVector(vector<int> v);
 
 int main(int argc, char const *argv[]) {
   vector<int> input1 = {1, NULL, 3, 2, 4, NULL, 5, 6};
   vector<int> input2 = {1, NULL, 2, 3, 4, 5, NULL, NULL, 6, 7, NULL, 8, NULL, 9, 10, NULL, NULL, 11, NULL, 12, NULL, 13, NULL, NULL, 14};
 
-  // Node *tree_root1 = createTree(input1);
+  Node *tree_root1 = createTree(input1);
   Node *tree_root2 = createTree(input2);
 
-    return 0;
+  // vector<int> preorderTraversal1 = preorder(tree_root1);
+  // vector<int> preorderTraversal2 = preorder(tree_root2);
+
+  // printVector(preorderTraversal1);
+  return 0;
 }
 
 vector<int> preorder(Node *root) {
   vector<int> pre;
+  Node *node, *childNode;  // traverses the node;
+  stack<Node *> s;
+  int i = 0;  // iterator for for loop
+
+  s.push(root);
+  node = root;
+
+  while (!s.empty()) {
+    // get the top most element on the stack
+    node = s.top();
+    s.pop();
+
+    // add node's val to the pre vector and
+    pre.push_back(node->val);
+
+    // add node's children to stack
+    for (i = node->children.size() - 1; i >= 0; i--) {
+      s.push(node->children[i]);
+    }
+  }
   return pre;
 }
 
@@ -62,10 +88,18 @@ Node *createTree(vector<int> input) {
       node = new Node(input[i]);
       nodes.push(node);
       parent->children.push_back(node);
-      cout << "adding " << node->val << " to " << parent->val << endl;
+      // cout << "adding " << node->val << " to " << parent->val << endl;
     } else {
       parent = nodes.front();
       nodes.pop();
     }
   }
+}
+
+// prints the contents of a vector
+void printVector(vector<int> v) {
+  for (int num : v) {
+    cout << num << " ";
+  }
+  cout << endl;
 }
