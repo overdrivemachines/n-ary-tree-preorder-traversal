46e6237448764df4d40f32fc36fb50655fa65229
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-04 18:34:58 -0700

    created tree given input vector

--- n-ary-tree-preorder-traversal.cpp
+++ n-ary-tree-preorder-traversal.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 
 using namespace std;
@@ -21,11 +22,50 @@ class Node {
   }
 };
 
+Node *createTree(vector<int> input);
 vector<int> preorder(Node *root);
 
 int main(int argc, char const *argv[]) {
-  return 0;
+  vector<int> input1 = {1, NULL, 3, 2, 4, NULL, 5, 6};
+  vector<int> input2 = {1, NULL, 2, 3, 4, 5, NULL, NULL, 6, 7, NULL, 8, NULL, 9, 10, NULL, NULL, 11, NULL, 12, NULL, 13, NULL, NULL, 14};
+
+  // Node *tree_root1 = createTree(input1);
+  Node *tree_root2 = createTree(input2);
+
+    return 0;
 }
 
 vector<int> preorder(Node *root) {
+  vector<int> pre;
+  return pre;
+}
+
+// Create n-ary tree
+// returns pointer to root
+Node *createTree(vector<int> input) {
+  queue<Node *> nodes;
+  Node *node, *parent, *root;
+
+  if (input.size() == 0) {
+    return nullptr;
+  }
+
+  root = new Node(input[0]);
+
+  if (input.size() == 1) {
+    return root;
+  }
+
+  parent = root;
+  for (int i = 2; i < input.size(); i++) {
+    if (input[i] != NULL) {
+      node = new Node(input[i]);
+      nodes.push(node);
+      parent->children.push_back(node);
+      cout << "adding " << node->val << " to " << parent->val << endl;
+    } else {
+      parent = nodes.front();
+      nodes.pop();
+    }
+  }
 }
